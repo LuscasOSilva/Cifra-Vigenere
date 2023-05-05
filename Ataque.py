78c3874b6857c11e111a75b3909d8a6f75e8cd05
Lucas de Oliveira Silva <lucasoliver3500@gmail.com>
2023-05-04 21:16:59 -0300

    Tentativa de descobrir tamanho da chave

--- Ataque.py
+++ Ataque.py
@@ -1,27 +1,28 @@
 import re
 
-mensagem = input("Digite a mensagem: ").upper() #Converte para maiúsculas
+mensagem = ''.join(filter(str.isalpha, input("Digite a mensagem: ").upper())) #Converte para maiúsculas
 
 repeticoes = {}
+tamanhos = [0]*19
 
 for i in range(len(mensagem) - 2):
     sequencia = mensagem[i:i+3]
-    if sequencia.isalpha(): #confere se os tres caracteres são letras
-        if sequencia in repeticoes:
-            repeticoes[sequencia]["quantidade"] += 1
-            repeticoes[sequencia]["ultima_ocorrencia"] = i
-            distancia = i - repeticoes[sequencia]["penultima_ocorrencia"]
-            if 1 <= distancia <= 20:
-                repeticoes[sequencia]["distancias"].append(distancia)
-            repeticoes[sequencia]["penultima_ocorrencia"] = i
-        else:
-            repeticoes[sequencia] = {
-                "quantidade": 1,
-                "penultima_ocorrencia": i,
-                "ultima_ocorrencia": i,
-                "distancias": []
-            }
-
+    if sequencia in repeticoes:
+        repeticoes[sequencia]["quantidade"] += 1
+        distancia = i - repeticoes[sequencia]["ultima_ocorrencia"]
+        for tamanho_chave in reversed(range(len(tamanhos))):
+            if distancia % (tamanho_chave + 2) == 0:
+                tamanhos[tamanho_chave] += 1
+                distancia = tamanho_chave/2
+        repeticoes[sequencia]["ultima_ocorrencia"] = i+3
+    else:
+        repeticoes[sequencia] = {
+            "quantidade": 1,
+            "ultima_ocorrencia": i+3
+        }
+print(tamanhos)
+'''
 for sequencia in repeticoes:
     if repeticoes[sequencia]["quantidade"] > 1:
         print(f"{sequencia}")
+'''
